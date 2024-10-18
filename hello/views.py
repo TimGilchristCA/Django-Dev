f26696c5d7d226c6921d7752c4d54d00d19ef93a
Andrew John Timothy Gilchrist <tim.gilchrist@chelsea-academy.org>
2024-10-18 07:56:18 +0000

    Changes to views.py

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, Django! Another change from VS code")
+    return HttpResponse("Hello, Django! Test change from codespace")
