8300bab9ee070fcc125e81e09732b80a82a50fde
Tim Gilchrist <tim.gilchrist@chelsea-academy.org>
2024-10-18 10:23:53 +0100

    Django tutorial update

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,21 @@
+
+import re
+from django.utils.timezone import datetime
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def home(request):
     return HttpResponse("Hello, Django! Test change from VScode")
+
+def hello_there(request, name):
+    print(request.build_absolute_uri()) #optional
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
+
